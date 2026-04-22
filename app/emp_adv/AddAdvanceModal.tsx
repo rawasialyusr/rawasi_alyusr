4577e6990df72a3ca1e6182a11b79b98bb188f9e
moo <moo.yasser.93@gmail.com>
2026-04-22 07:54:38 +0200

    zzzzz

--- app/emp_adv/AddAdvanceModal.tsx
+++ app/emp_adv/AddAdvanceModal.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { createPortal } from 'react-dom'; // 👈 استيراد البورتال
 import { THEME } from '@/lib/theme';
 import SmartCombo from '@/components/SmartCombo';
 
